a92b37c557f3ec7e6f35965177c60498ce495a00
Joaquim Perez <jivkim@gmail.com>
2023-01-08 16:46:07 +0100

    Add index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.classless.min.css">
+    <title>Shared Textarea</title>
+  </head>
+  <body>
+    <main>
+      <h1 style="text-align:center">Shared Textarea</h1>
+      <textarea rows="10">Hello, world!</textarea>
+    </main>
+  </body>
+</html>
